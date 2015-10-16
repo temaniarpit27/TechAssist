f64beb1055579e6ec35193d8c8c77f21760cfed0
Ashutosh Gupta <ashsun21292@gmail.com>
2015-10-16 14:10:53 +0530

    validations

--- db/migrate/20151016082154_create_user_repo_joins.rb
+++ db/migrate/20151016082154_create_user_repo_joins.rb
@@ -1,8 +1,8 @@
 class CreateUserRepoJoins < ActiveRecord::Migration
   def change
     create_table :user_repo_joins do |t|
-      t.integer :user_id
-      t.integer :repo_id
+      t.integer :user_id , null: false
+      t.integer :repo_id , null: false
 
       t.timestamps null: false
     end
