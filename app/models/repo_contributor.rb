974db54328f301842ac589949bc57a6f29961020
Jatin Chuglani <chuglani.jatin@gmail.com>
2015-10-17 01:09:09 +0530

    added experts api

--- app/models/repo_contributor.rb
+++ app/models/repo_contributor.rb
@@ -9,19 +9,19 @@ class RepoContributor < ActiveRecord::Base
       url = URI("https://api.github.com/repos/loconsolutions/#{repo.name}/contributors")
       req = Net::HTTP::Get.new(url)
       req['Content-Type'] = "application/json"
-      req['Authorization'] = 'token '
+      req['Authorization'] = 'token 03a8bf85550eb3ac15ce0cab401c65af6e017106'
       req['Accept'] = 'application/json'
     
       res = Net::HTTP.start(url.hostname, url.port,:use_ssl => true) {|http|
         http.request(req)
       }
-
+      puts res.body
       body = JSON.parse(res.body)
       body.each do |contributor|
       	#puts contributor
       	user = User.find_by(:name => contributor["login"]) 
-      	if user
-        	RepoContributor.create({:user_id => user.id , :repository_id => repo.id})
+      	if !user.nil?
+        	RepoContributor.create({:user_id => user.id , :repository_id => repo.id, :contributions => contributor["contributions"]})
     	end
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'experts' => 'repositories#get_experts'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
