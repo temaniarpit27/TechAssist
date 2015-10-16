47f7e27b7310cabe56de07f39c7c596a030ce6bc
Arpit Temani <temaniarpit27@gmail.com>
2015-10-16 23:55:03 +0530

    changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,12 +5,14 @@ class Comment < ActiveRecord::Base
   after_save :create_notification
 
   def create_notification
-    if self.entity_type == "Answer"
-      text = "You have a comment on your answer #{self.entity.answer[0...20]}..."
-      Notification.create(:user_id => self.entity.user_id, :question_id => self.entity.question_id, :text => text)
-    else
-      text = "You have a comment on question #{self.entity.title[0...20]}..."
-      Notification.create(:user_id => self.entity.user_id, :question_id => self.entity_id, :text => text)
+    unless self.user_id == self.entity.user_id
+      if self.entity_type == "Answer"
+        text = "You have a comment on your answer #{self.entity.answer[0...20]}..."
+        Notification.create(:user_id => self.entity.user_id, :question_id => self.entity.question_id, :description => text)
+      else
+        text = "You have a comment on question #{self.entity.title[0...20]}..."
+        Notification.create(:user_id => self.entity.user_id, :question_id => self.entity_id, :description => text)
+      end
     end
   end
 end
