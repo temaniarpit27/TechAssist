494e713bf2c2585536ccdcad0fb6dfa7e1033fe0
Arpit Temani <temaniarpit27@gmail.com>
2015-10-16 14:33:42 +0530

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016081305) do
+ActiveRecord::Schema.define(version: 20151016082154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20151016081305) do
     t.string   "email"
     t.string   "password"
     t.boolean  "set_flag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
